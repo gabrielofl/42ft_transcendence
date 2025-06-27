7a3b11fbdb8694386a1993ccc93967ee1c7ca565
gabrielofl <110313809+gabrielofl@users.noreply.github.com>
2025-06-27 20:07:30 +0200

    feat. hot reload fixed -use http://localhost:8080

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -8,6 +8,16 @@ export default defineConfig(({ mode }) => {
     build: {
       outDir: '../dist',
       emptyOutDir: true,
-    },
+	  },
+	server: {
+    host: '0.0.0.0',
+    port: 5173,
+		strictPort: true,
+	allowedHosts: ['frontend'],
+	watch: {
+    usePolling: true,
+		interval: 100
+  }
+  }
   };
 });
