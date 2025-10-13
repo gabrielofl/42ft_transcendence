6bbe9aa612810ac086de468baa24cad89b27ccf6
Miguel <migclay12@gmail.com>
2025-10-13 14:08:46 +0200

    comment

--- frontend/src/screens/user-card.ts
+++ frontend/src/screens/user-card.ts
@@ -1,4 +1,5 @@
 import { replaceTemplatePlaceholders } from "./utils";
+//DESCOMENTAR MAS TARDE
 //import userCardTemplate from "./user_card.html?raw";
 import { API_BASE_URL } from "./config";
 import { UserData } from "@shared/types/messages";
