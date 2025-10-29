3ad0c2706d33d862b4a3eec5c73945052d7b1070
set <dm.daviles@gmail.com>
2025-10-29 20:53:03 +0100

    Update footer links.@

--- frontend/src/components/Footer.ts
+++ frontend/src/components/Footer.ts
@@ -9,10 +9,12 @@ export function renderFooter(): void {
   			<!-- Divider -->
 		<div class="relative">
     <footer	class="w-full h-1/10 fixed bottom-0 bg-main border-t border-[--primary-color] font-press text-white px-6 py-4 flex justify-between items-start ">
-                <div class="text-lg font-press font-bold tracking-wide flex items-center gap-2">
+			<a href="#" id="nav-home" class="hover:underline">
+				<div class="text-lg font-press font-bold tracking-wide flex items-center gap-2">
 				<span class="material-symbols-outlined text-3xl">
 				swords
 				</span> Pong</div>
+			</a>
 				
       <div class="max-w-6xl mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-5 gap-8">
         <!-- Logo + Socials -->
@@ -26,30 +28,33 @@ export function renderFooter(): void {
         </div>
 
         <!-- Play Game -->
-        <div class="text-[--secondary-color] text-xs">
+        <div class="text-[--text-light] text-xs">
           <h3 class="font-bold mb-2">Play game</h3>
-          <ul class="space-y-1">
-            <li><a href="#" id="nav-home" class="hover:underline">1p vs 1p</a></li>
-            <li><a href="#" class="hover:underline">2p vs 2p</a></li>
-            <li><a href="#" class="hover:underline">1p vs AI</a></li>
+          <ul class="space-y-1 text-[--secondary-color]">
+            <li><a href="#" id="nav-create-game" class="hover:underline">Create Game</a></li>
+            <li><a href="#" id="nav-join-game"class="hover:underline">Join Game</a></li>
+            <li><a href="#" id="nav-tournament-game" class="hover:underline">Tournament Mode</a></li>
           </ul>
         </div>
 
         <!-- User -->
-        <div class="text-[--secondary-color] text-xs">
+        <div class="text-[--text-light] text-xs">
           <h3 class="font-bold mb-2">User</h3>
-          <ul class="space-y-1">
-            <li><a href="#" id="nav-profile" class="hover:underline">Profile</a></li>
-            <li><a href="#" id="nav-logout" class="hover:underline">Leaderboard</a></li>
+          <ul class="space-y-1 text-[--secondary-color]">
+            <li><a href="#" id="nav-profile" class="hover:underline">Account</a></li>
+            <li><a href="#" id="nav-friends" class="hover:underline">Friends</a></li>
+            <li><a href="#" id="nav-match-history" class="hover:underline">Match History</a></li>
+            <li><a href="#" id="nav-logout" class="hover:underline">Logout</a></li>
           </ul>
         </div>
 
         <!-- Contact -->
-        <div class="text-[--secondary-color] text-xs">
-          <h3 class="font-bold mb-2 ">Contact</h3>
-          <ul class="space-y-1">
-            <li><a href="#" class="hover:underline">Contact us</a></li>
-            <li><a href="https://github.com" target="_blank" class="hover:underline">Github</a></li>
+        <div class="text-[--text-light] text-xs">
+          <h3 class="font-bold mb-2 ">Global</h3>
+          <ul class="space-y-1  text-[--secondary-color]">
+            <li><a href="#" id="nav-leaderboard" class="hover:underline">Leaderboard</a></li>
+			<li><a href="#" id="nav-contact" class="hover:underline">Contact us</a></li>
+            <li><a href="https://github.com/gabrielofl/42ft_transcendence" target="_blank" class="hover:underline">Github</a></li>
           </ul>
         </div>
       </div>
@@ -63,6 +68,24 @@ export function renderFooter(): void {
     e.preventDefault();
     navigateTo("home");
   });
+ 
+  const createGameBtn = document.getElementById("nav-create-game");
+  createGameBtn?.addEventListener("click", (e) => {
+    e.preventDefault();
+    navigateTo("create");
+  });
+
+  const joinGameBtn = document.getElementById("nav-join-game");
+  joinGameBtn?.addEventListener("click", (e) => {
+    e.preventDefault();
+    navigateTo('join');
+  });
+
+  const tournamentGameBtn = document.getElementById("nav-tournament-game");
+  tournamentGameBtn?.addEventListener("click", (e) => {
+    e.preventDefault();
+    navigateTo("tournament");
+  });
 
   const profileBtn = document.getElementById("nav-profile");
   profileBtn?.addEventListener("click", (e) => {
@@ -70,15 +93,34 @@ export function renderFooter(): void {
     navigateTo("profile");
   });
 
+  const friendBtn = document.getElementById("nav-friends");
+  friendBtn?.addEventListener("click", (e) => {
+    e.preventDefault();
+    navigateTo("profile");
+  });
+
+  const matchHistoryBtn = document.getElementById("nav-match-history");
+  matchHistoryBtn?.addEventListener("click", (e) => {
+    e.preventDefault();
+    navigateTo("profile");
+  });
+
+    const leaderboardBtn = document.getElementById("nav-leaderboard");
+  leaderboardBtn?.addEventListener("click", (e) => {
+	e.preventDefault();
+	navigateTo("leaderboard");
+  });
+  
   const logoutBtn = document.getElementById("nav-logout");
   logoutBtn?.addEventListener("click", async (e) => {
-    e.preventDefault();
+	  e.preventDefault();
     try {
-      await apiService.logout();
+		await apiService.logout();
       navigateTo("login");
     } catch (error) {
       console.error('Logout error:', error);
       navigateTo("login");
     }
   });
+  
 }
